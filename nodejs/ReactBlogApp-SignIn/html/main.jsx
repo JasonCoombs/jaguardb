a7df844c576a2b79ef8e6b2333a5fb00b6ea0a50
LYN <lyn9721@gmail.com>
2018-10-04 14:12:58 -0700

    project create

---
+++ nodejs/ReactBlogApp-SignIn/html/main.jsx
@@ -0,0 +1,57 @@
+var Router = window.ReactRouter.Router;
+var Route = window.ReactRouter.Route;
+var hashHistory = window.ReactRouter.hashHistory;
+var Link = window.ReactRouter.Link;
+
+class Signin extends React.Component {
+    constructor(props) {
+      super(props);
+      this.signIn = this.signIn.bind(this);
+      this.handleEmailChange = this.handleEmailChange.bind(this);
+      this.handlePasswordChange = this.handlePasswordChange.bind(this);
+      this.state = {
+        email:'',
+        password:''
+      };
+    }
+
+    signIn(){
+      axios.post('/signin', {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+
+    handleEmailChange(e){
+      this.setState({email:e.target.value})
+    }
+    handlePasswordChange(e){
+      this.setState({password:e.target.value})
+    }
+    render() {
+      return (
+        <div>
+          <form className="form-signin">
+            <h2 className="form-signin-heading">Please sign in</h2>
+            <label for="inputEmail" className="sr-only">Email address</label>
+            <input type="email" onChange={this.handleEmailChange} id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+            <label for="inputPassword" className="sr-only">Password</label>
+            <input type="password" onChange={this.handlePasswordChange} id="inputPassword" className="form-control" placeholder="Password" required />
+            <button className="btn btn-lg btn-primary btn-block" onClick={this.signIn} type="button">Sign in</button>
+          </form>
+        </div>
+      )
+    }
+}
+
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route component={Signin} path="/"></Route>
+    </Router>,
+document.getElementById('app'));
